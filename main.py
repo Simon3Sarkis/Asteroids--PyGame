8a32dd9160ae8861dcdc41e656a004d992411f73
Simon3Sarkis <simon.sarkis3@gmail.com>
2025-07-09 22:20:16 -0400

    Initial commit 🎮 Start project

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from constants import *
+from player import Player
+
+def main():
+    
+    pygame.init()
+
+    clock =pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids Game")
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return  
+        player.update(dt)
+        screen.fill((0, 0, 0))
+        player.draw(screen) 
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
+        
+if __name__ == "__main__":
+    main()
